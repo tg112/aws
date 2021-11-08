0f53089c533d576cefb646c5a40242101c9a2ad8
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2021-11-08 09:57:01 +0900

    add: template

--- serverless-express/app.js
+++ serverless-express/app.js
@@ -3,6 +3,8 @@ const app = express();
 
 const router = require('./routes/router');
 
+app.set("view engine", "ejs");
+
 app.use('/', router);
 
 module.exports = app;
--- serverless-express/routes/router.js
+++ serverless-express/routes/router.js
@@ -2,10 +2,14 @@ const express  = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({message : v4()});
+    res.json({message : "get"});
 });
 router.get('/users', (req, res) => {
     res.json([{name: 'Taro'}, {name: 'Hanako'}]);
 });
+
+router.get("/test", (req, res) => {
+    res.render("index.ejs");
+})
 
 module.exports = router;
