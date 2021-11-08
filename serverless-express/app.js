e371781fb4a20a669b410da75ad903a8f346740a
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2021-11-08 14:34:20 +0900

    add: connected db

--- serverless-express/app.js
+++ serverless-express/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const db = require("./db/database");
 const app = express();
+require("dotenv").config({ path: "variables.env" });
+
+db.authenticate()
+    .then(() => {
+        console.log("Database connected...")
+    })
+    .catch(e => console.log(e.message))
+
 
 const alertRoutes = require('./src/routes/alerts');
 const viewRoutes = require('./src/routes/views');
