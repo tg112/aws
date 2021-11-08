e003720a1ca2c8bea050e055e9d23584a62ad2fb
Shota Togawa <shotatogawa@ShotanoMacBook-Pro.local>
2021-11-08 10:31:05 +0900

    fix: リージョンとpostmethod

--- serverless-express/app.js
+++ serverless-express/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
 
 const router = require('./routes/router');
 
 app.set("view engine", "ejs");
-
+app.use(express.json());
+app.use(cors());
 app.use('/', router);
 
 module.exports = app;
--- serverless-express/routes/router.js
+++ serverless-express/routes/router.js
@@ -4,8 +4,9 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.json({message : "get"});
 });
-router.get('/users', (req, res) => {
-    res.json([{name: 'Taro'}, {name: 'Hanako'}]);
+router.post('/users', (req, res) => {
+    const { name, time } = req.body;
+    res.status(200).send({ name, time })
 });
 
 router.get("/test", (req, res) => {
